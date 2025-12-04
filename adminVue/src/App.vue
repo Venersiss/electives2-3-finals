e47a2dad652660b02edc54b9964a463eb9703ca0
Venersis <kent200417@gmail.com>
2025-12-04 17:19:08 +0800

    added vuetify

--- adminVue/src/App.vue
+++ adminVue/src/App.vue
@@ -4,20 +4,17 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-main>
+      
+      <v-container>
+        <v-btn color="success">
+          Vuetify Test Button
+        </v-btn>
+      </v-container>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
--- adminVue/src/main.js
+++ adminVue/src/main.js
@@ -3,6 +3,31 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// --- START VUETIFY CONFIGURATION ---
+
+// 1. Import Vuetify styles and core functions
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi' 
+
+// 2. Initialize Vuetify instance
+const vuetify = createVuetify({
+  components,
+  directives,
+  // Configure the default icon set
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+// --- END VUETIFY CONFIGURATION ---
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +35,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// 3. Register the Vuetify instance with the Vue app
+app.use(vuetify) 
 
 app.mount('#app')
